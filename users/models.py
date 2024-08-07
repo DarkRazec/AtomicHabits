d4e0cdd2a779f440fab7c8b5c9e19acfa5361b98
DarkRazec <darkrazec@yahoo.com>
2024-08-07 20:18:08 +0300

    add models

---
+++ users/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+    tg_chat_id = models.CharField(max_length=15, verbose_name='ID чата пользователя')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('first_name',)
